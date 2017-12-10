2a88617ccdc4305dd3735cfd2fd66794337bea72
VladislavTitov <titovvlad1997@yandex.ru>
2017-12-10 19:40:57 +0300

    AnnotationConverter: added convert from object to object

--- src/main/java/ru/kpfu/itis/converter/AnnotationConverter.java
+++ src/main/java/ru/kpfu/itis/converter/AnnotationConverter.java
@@ -13,18 +13,25 @@ public class AnnotationConverter {
 
     @Nullable
     public static <T> T convert(Object from, Class<T> to) {
-        Map<String, Field> fieldsMap = new HashMap<>();
-        Class fromClass = from.getClass();
         T toInstance = null;
         try {
             toInstance = to.newInstance();
         } catch (InstantiationException | IllegalAccessException e) {
             e.printStackTrace();
         }
+        return convert(from, toInstance);
+    }
+
+    @Nullable
+    public static <T> T convert(Object from, T toInstance) {
         if (toInstance == null) {
             return null;
         }
 
+        Map<String, Field> fieldsMap = new HashMap<>();
+        Class fromClass = from.getClass();
+
+        Class to = toInstance.getClass();
         for (Field toField : to.getDeclaredFields()) {
             SharedField sharedField = toField.getDeclaredAnnotation(SharedField.class);
             if (sharedField != null) {
@@ -32,15 +39,27 @@ public class AnnotationConverter {
             }
         }
 
+
         for (Field fromField : fromClass.getDeclaredFields()) {
+            fromField.setAccessible(true);
+            try {
+                if (fromField.get(from) == null) {
+                    continue;
+                }
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+                continue;
+            }
             SharedField sharedField = fromField.getDeclaredAnnotation(SharedField.class);
             if (sharedField != null) {
                 Field toField = fieldsMap.get(sharedField.name());
+                if (toField == null) {
+                    continue;
+                }
+                toField.setAccessible(true);
                 if (!toField.getType().equals(fromField.getType())) {
                     throw new TypeMismatchException("Type of " + fromClass.getCanonicalName() + "." + fromField.getName() + " not equals " + to.getCanonicalName() + "." + toField.getName());
                 }
-                fromField.setAccessible(true);
-                toField.setAccessible(true);
                 try {
                     toField.set(toInstance, fromField.get(from));
                 } catch (IllegalAccessException e) {
