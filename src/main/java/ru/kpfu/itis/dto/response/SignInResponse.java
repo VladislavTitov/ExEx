2994c57694aa604b13681eddcc4130cf3e425337
VladislavTitov <titovvlad1997@yandex.ru>
2017-12-11 01:27:48 +0300

    added SignInResponse model

---
+++ src/main/java/ru/kpfu/itis/dto/response/SignInResponse.java
@@ -0,0 +1,73 @@
+package ru.kpfu.itis.dto.response;
+
+import java.util.HashMap;
+import java.util.Map;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+        "id",
+        "token"
+})
+public class SignInResponse {
+
+    @JsonProperty("id")
+    private Long id;
+    @JsonProperty("token")
+    private String token;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    /**
+     * No args constructor for use in serialization
+     *
+     */
+    public SignInResponse() {
+    }
+
+    /**
+     *
+     * @param id
+     * @param token
+     */
+    public SignInResponse(Long id, String token) {
+        this.id = id;
+        this.token = token;
+    }
+
+    @JsonProperty("id")
+    public Long getId() {
+        return id;
+    }
+
+    @JsonProperty("id")
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @JsonProperty("token")
+    public String getToken() {
+        return token;
+    }
+
+    @JsonProperty("token")
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+}
