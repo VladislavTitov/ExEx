625f8ea32e6faa4aed26e47f6bdc813e11177c1e
VladislavTitov <titovvlad1997@yandex.ru>
2018-04-04 04:09:48 +0300

    added new converter test [with new list converting]

--- src/test/java/ru/kpfu/itis/SimpleTest.java
+++ src/test/java/ru/kpfu/itis/SimpleTest.java
@@ -4,8 +4,15 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import ru.kpfu.itis.converter.AnnotationConverter;
+import ru.kpfu.itis.dto.common.SingleCourse;
 import ru.kpfu.itis.dto.request.SignUpRequest;
+import ru.kpfu.itis.model.Course;
+import ru.kpfu.itis.model.Interest;
 import ru.kpfu.itis.model.User;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class SimpleTest {
 
@@ -13,11 +20,31 @@ public class SimpleTest {
     User user;
     User mutableUser;
 
+    Course course;
+    SingleCourse targetCourse;
+
     @Before
     public void setUp() {
         signUpRequest = new SignUpRequest("a", "b", "c", "d", "e");
         user = new User("a", "b", "c", "d", "e");
         mutableUser = new User("m", null, null, null, null);
+
+
+        Interest interest1 = new Interest();
+        interest1.setId(1L);
+        Interest interest2 = new Interest();
+        interest2.setId(2L);
+        List<Interest> interests = new ArrayList<>(Arrays.asList(interest1, interest2));
+
+        List<Long> interestIds = new ArrayList<>(Arrays.asList(1L, 2L));
+
+        course = new Course("testTitle", "testSummary", "testCover", 0, 0, null, null);
+        course.setId(1L);
+        course.setInterests(interests);
+        targetCourse = new SingleCourse("testTitle", "testSummary", "testCover", 0, 0);
+        targetCourse.setId(1L);
+        targetCourse.setLikersNumber(0);
+        targetCourse.setInterests(interestIds);
     }
 
     @Test
@@ -31,6 +58,12 @@ public class SimpleTest {
     public void convertObjectToObject() {
         AnnotationConverter.convert(signUpRequest, mutableUser);
         Assert.assertEquals(user, mutableUser);
+    }
+
+    @Test
+    public void converterWithListsTest() {
+        SingleCourse actualCourse = AnnotationConverter.convert(course, SingleCourse.class);
+        Assert.assertEquals(targetCourse, actualCourse);
     }
 
 }
